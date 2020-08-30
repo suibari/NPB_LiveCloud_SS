6a24e834331aedda38b60848b010821db05b10d7
suibari <404.not.found.address@gmail.com>
2020-08-30 15:48:52 +0900

    divide twi-lyser (tmp)

--- app.js
+++ app.js
@@ -14,81 +14,66 @@ const teams = [
   "hawks", "lions", "buffaloes", "eagles", "fighters", "marines"
 ];
 
-// twitterストリーミングエンドポイントとの接続
-const stream = twit.stream('statuses/filter',
-  {
-    track: convertAry( require('./src/query.json') ), 
-    language: "ja"
-  }
-);
-
-// ツイートがあるたびにanalysis.jsを呼び出す
-stream.on('tweet', (tweet) => {
-  console.log(tweet.user.name + "> " + tweet.text);
-  // analysis.jsにtweet.textを渡す
-  require('./analysis.js')(tweet.text);
-});
-
 // wordCloud描画インスタンス作成
-const cloud = new wordcloud.wordCloud();
+const cloud = new wordcloud.wordCloud(1000, 1000);
 
 // ある時間にcron_botでWC生成＆ツイート
-//cron.schedule('*/10 * * * * *', () => { // 10秒おきに実行
-cron.schedule('0 0 9,18,23 * * *', () => { // 9時、18時、23時に実行
+cron.schedule('*/30 * * * * *', () => { // 10秒おきに実行
+//cron.schedule('0 0 9,18,23 * * *', () => { // 9時、18時、23時に実行
 
   // SVG取得
-  cloud.getSvg(require('./storing.js').getCount("all")).then((svg) => {
-    console.log("YUKI.N > successful get SVG.");
-    
-    // svg -> base64変換
-    svg2(svg).toUri({ base64: true, mime: svg2.PNG }).then((uri) => {
-    //svg2(svg).jpeg().toFile("./test.jpeg").then((uri) => {
-      //console.log(uri); // iVBORw0KGgoAAAANSUhEUgAAADAAAAA...
-      console.log("YUKI.N > -----------------------------------------")
-      console.log("YUKI.N > successful svg2base64 conversion.");
-      console.log("YUKI.N > -----------------------------------------")
-      // twitter投稿
-      // 1. 画像アップロード(base64)
-      twit.post('media/upload', { media_data: uri }, function (err, data, response) {
-        
-        // 2. 画像付きツイート
-        let text = "NPBライブクラウドbotがお知らせします。\n" +
-                   "各球団で盛り上がっているワードは以下です。\n";
-        teams.forEach((team) => {
-          let count = require('./storing.js').getCount(team);
-          if (count.length > 0) {
-            text += "#" + team + " :" + count[count.length - 1].word + "\n";
-          }
+  require('./redis_wrap.js').getCount("all", 200).then((words) => { 
+
+    //console.log(words)
+    cloud.getSvg(words).then((svg) => {
+      console.log("YUKI.N > -----------------------------------------");
+      console.log("YUKI.N > successful get SVG.");
+      console.log("YUKI.N > -----------------------------------------");
+      
+      // svg -> base64変換
+      svg2(svg).toUri({ base64: true, mime: svg2.PNG }).then((uri) => {
+      //svg2(svg).jpeg().toFile("./test.jpeg").then((uri) => {
+        //console.log(uri); // iVBORw0KGgoAAAANSUhEUgAAADAAAAA...
+        console.log("YUKI.N > -----------------------------------------");
+        console.log("YUKI.N > successful svg -> base64 conversion.");
+        console.log("YUKI.N > -----------------------------------------");
+  
+        // twitter投稿
+        // 1. 画像アップロード(base64)
+        twit.post('media/upload', { media_data: uri }, function (err, data, response) {
+          
+          // 2. 画像付きツイート
+          var text = "現在の球界の話題はこんな感じです。\n";
+          teams.forEach( async (team) => {
+            await require('./redis_wrap.js').getCount(team, 1).then((count_team) => {
+              text += "#" + team + " :" + count_team[0].word + "\n";
+            });
+          });
+          text += "URL: https://npb-livecloud.herokuapp.com/";
+          console.log(text);
+          var params = { status: text, media_ids: [data.media_id_string] }
+  
+          twit.post('statuses/update', params, function (err, data, response) {
+            if (!err) {
+              console.log("YUKI.N > =====================================")
+              console.log("YUKI.N > successsful post to Twitter.")
+              console.log("YUKI.N > =====================================")
+  
+            } else {
+              console.error(err)
+            }
+          });
         });
-        text += "\n"+
-                "URL: https://npb-livecloud.herokuapp.com/";
-        console.log(text);
-        var params = { status: text, media_ids: [data.media_id_string] }
-        twit.post('statuses/update', params, function (err, data, response) {
-          if (!err) {
-            console.log("YUKI.N > =====================================")
-            console.log("YUKI.N > successsful post to Twitter.")
-            console.log("YUKI.N > =====================================")
-          } else {
-            console.error(response)
-          }
-        });
+      })
+      .catch(err => { // svg変換失敗したらthrow
+          throw err;
       });
-    })
-    .catch((err) => { // svg変換失敗したらthrow
-        throw err;
     });
-
+  })
+  .catch(err => { // redisアクセス失敗したらthrow
+    throw err;
   });
-})
+});
 
-// JSONから配列にパースする関数
-function convertAry(json) {
-  var q = [];
-  for (let key in json) {
-    for (let i = 0; i < json[key].length; i++) {
-      q.push(json[key][i]);
-    }
-  }
-  return q;
-};
+const SegfaultHandler = require('segfault-handler');
+SegfaultHandler.registerHandler('crash.log')
---
+++ redis_wrap.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// redisインスタンス生成&接続
+const Redis = require('ioredis');
+const redis = new Redis(process.env.REDIS_URL);
+
+module.exports.getCount = async function (team, num) {
+  let arr_count_all  = [];
+  let arr_count_team = [];
+
+  if (team == "all") {
+    // ----全チーム選択時----
+    // zrevrangebyscoreを取って、JSON.parseしてオブジェクトに変換する
+    (await redis.zrevrangebyscore("count_all", "+inf", 1, "WITHSCORES", "LIMIT", 0, num, (err, res) => {
+      // redisのresultをwordcloud作りやすいフォーマットに変換
+      arr_count_all = convertRedisArr(res);
+      // arr_count_all.wordに対してJSON.parseした[0]をwordに再代入し、[1]をteamプロパティに入れる
+      arr_count_all.forEach((count_all) => {
+        const arr_wt = JSON.parse(count_all.word);
+        count_all.word = arr_wt[0];
+        count_all.team = arr_wt[1];
+      });
+    }));
+    return arr_count_all;
+
+  } else {
+    // ----単体チーム選択時----
+    // zrevrangebyscoreを取って、オブジェクトに変換する
+    (await redis.zrevrangebyscore("count_"+team, "+inf", 1, "WITHSCORES", "LIMIT", 0, num, (err, res) => {
+      // redisのresultをwordcloud作りやすいフォーマットに変換
+      arr_count_team = convertRedisArr(res);
+      // チーム名を全要素にセット
+      arr_count_team.forEach((obj) => {
+        obj.team = team;
+      });
+    }));
+    return arr_count_team;
+  }
+};
+
+module.exports.getTPS = function () {
+  redis.get("tps", (err, res) => {
+    if (!err) {
+      return JSON.parse(res);
+    } else {
+      console.log(err);
+    }
+  })
+};
+
+// redisのresultを変換する関数
+function convertRedisArr (arr) {   // ex.) [0, 1, 2, 3, ...]
+  let res  = [];
+  let even = [];
+  let odd  = [];
+
+  even = arr.filter((value, index) => {
+    return index % 2 === 0; // [0, 2, 4, ...]
+  });
+  odd = arr.filter((value, index) => {
+    return index % 2 === 1; // [1, 3, 5, ...]
+  });
+
+  for (let i=0; i<even.length; i++) {
+    res.push(
+      { 
+        word:  even[i],
+        count: odd[i]
+      }
+    );
+  }
+
+  return res;
+}
